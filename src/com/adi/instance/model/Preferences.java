11b70f774ea68140e08d1dfe5987751844de38bd
Caique Siqueira <cslclaman@gmail.com>
2017-10-21 19:06:40 -0200

    Coloca nomes de atributos e métodos referentes a listas no plural.

--- src/com/adi/instance/model/Preferences.java
+++ src/com/adi/instance/model/Preferences.java
@@ -23,10 +23,10 @@ public class Preferences {
     private int version;
     private String theme;
     private String defaultSource;
-    private String[] databaseBackup;
-    private String[] inputFolder;
-    private String[] outputFolder;
-    private String[] outputCopy;
+    private String[] databaseBackups;
+    private String[] inputFolders;
+    private String[] outputFolders;
+    private String[] outputCopies;
 
     public String getIdent() {
         return ident;
@@ -60,52 +60,52 @@ public class Preferences {
         this.defaultSource = defaultSource;
     }
 
-    public String[] getDatabaseBackup() {
-        return databaseBackup;
+    public String[] getDatabaseBackups() {
+        return databaseBackups;
     }
     
-    public List<String> listDatabaseBackup(){
-        return Arrays.asList(databaseBackup);
+    public List<String> listDatabaseBackups(){
+        return Arrays.asList(databaseBackups);
     }
 
-    public void setDatabaseBackup(String[] databaseBackup) {
-        this.databaseBackup = databaseBackup;
+    public void setDatabaseBackups(String[] databaseBackups) {
+        this.databaseBackups = databaseBackups;
     }
 
-    public String[] getInputFolder() {
-        return inputFolder;
+    public String[] getInputFolders() {
+        return inputFolders;
     }
     
-    public List<String> listInputFolder(){
-        return Arrays.asList(inputFolder);
+    public List<String> listInputFolders(){
+        return Arrays.asList(inputFolders);
     }
 
-    public void setInputFolder(String[] inputFolder) {
-        this.inputFolder = inputFolder;
+    public void setInputFolders(String[] inputFolders) {
+        this.inputFolders = inputFolders;
     }
 
-    public String[] getOutputFolder() {
-        return outputFolder;
+    public String[] getOutputFolders() {
+        return outputFolders;
     }
 
-    public List<String> listOutputFolder(){
-        return Arrays.asList(outputFolder);
+    public List<String> listOutputFolders(){
+        return Arrays.asList(outputFolders);
     }
     
-    public void setOutputFolder(String[] outputFolder) {
-        this.outputFolder = outputFolder;
+    public void setOutputFolders(String[] outputFolders) {
+        this.outputFolders = outputFolders;
     }
 
-    public String[] getOutputCopy() {
-        return outputCopy;
+    public String[] getOutputCopies() {
+        return outputCopies;
     }
 
-    public List<String> listOutputCopy(){
-        return Arrays.asList(outputCopy);
+    public List<String> listOutputCopies(){
+        return Arrays.asList(outputCopies);
     }
     
-    public void setOutputCopy(String[] outputCopy) {
-        this.outputCopy = outputCopy;
+    public void setOutputCopies(String[] outputCopies) {
+        this.outputCopies = outputCopies;
     }
     
     public static Preferences defaultPreferences(){
@@ -120,16 +120,16 @@ public class Preferences {
         p.ident = host + "/" + System.getProperty("user.name");
         p.version = 1;
         p.theme = "Windows";
-        p.inputFolder = new String[]{
+        p.inputFolders = new String[]{
             Configuration.LOCAL_PATH + "default_input" + File.separator
         };
-        p.outputFolder = new String[]{
+        p.outputFolders = new String[]{
             Configuration.LOCAL_PATH + "default_output" + File.separator
         };
-        p.databaseBackup = new String[]{
+        p.databaseBackups = new String[]{
             Configuration.LOCAL_PATH + "default_dbbackup" + File.separator
         };
-        p.outputCopy = new String[0];
+        p.outputCopies = new String[0];
         p.defaultSource = "Danbooru";
         
         String[] folders = new String[]{
@@ -147,7 +147,7 @@ public class Preferences {
 
     @Override
     public String toString() {
-        return "Preferences{" + "ident=" + ident + ", version=" + version + ", theme=" + theme + ", defaultSource=" + defaultSource + ", databaseBackup=" + Arrays.toString(databaseBackup) + ", inputFolder=" + Arrays.toString(inputFolder) + ", outputFolder=" + Arrays.toString(outputFolder) + ", outputCopy=" + Arrays.toString(outputCopy) + '}';
+        return "Preferences{" + "ident=" + ident + ", version=" + version + ", theme=" + theme + ", defaultSource=" + defaultSource + ", databaseBackup=" + Arrays.toString(databaseBackups) + ", inputFolder=" + Arrays.toString(inputFolders) + ", outputFolder=" + Arrays.toString(outputFolders) + ", outputCopy=" + Arrays.toString(outputCopies) + '}';
     }
     
     

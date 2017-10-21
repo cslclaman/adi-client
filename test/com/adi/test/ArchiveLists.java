e9a7b512e3a51b3ac29d7eb195f7bd71173471b9
Caique Siqueira <cslclaman@gmail.com>
2017-10-21 21:44:10 -0200

    Adiciona classe ArchiveLists que testa listas e filtros de arquivos (Archiver).

---
+++ test/com/adi/test/ArchiveLists.java
@@ -0,0 +1,39 @@
+/*
+ * ADI 6 Project
+ * Copyright C.S.L. ClaMAN 2017
+ * This code is provided "as is". Modify this thing under your responsability.
+ */
+package com.adi.test;
+
+import com.adi.instance.Configuration;
+import com.adi.instance.model.Preferences;
+import com.adi.service.file.Archive;
+import com.adi.service.function.Archiver;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author Caique
+ */
+public class ArchiveLists {
+    public static void main(String[] args) throws IOException {
+        Configuration conf = new Configuration();
+        Archiver archiver = new Archiver(conf.getPreferences().getInputFolders(), true);
+        for (Archive a : archiver.getArchiveList()){
+            System.out.println(a.getPath());
+        }
+        System.out.println(archiver.archivesCount());
+        archiver.removeFolder("C:\\Users\\Caique\\Dropbox\\V_Imagens\\Twitter\\", true);
+        for (Archive a : archiver.getArchiveList()){
+            System.out.println(a.getPath());
+        }
+        System.out.println(archiver.archivesCount());
+        archiver.removeArchive("C:\\Users\\Caique\\Dropbox\\V_Imagens\\D_Gelbooru\\2b2081632ad72f5b76ed1f3f63d9e1e4.png");
+        for (Archive a : archiver.getArchiveList()){
+            System.out.println(a.getPath());
+        }
+        System.out.println(archiver.archivesCount());
+    }
+    
+}

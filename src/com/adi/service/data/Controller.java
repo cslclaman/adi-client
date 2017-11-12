164c015917e9319b6d1b753c40c333f7da6037ed
Caique Siqueira <cslclaman@gmail.com>
2017-11-12 17:09:22 -0200

    Add controller classes for CRUD things (using ADI-API).

---
+++ src/com/adi/service/data/Controller.java
@@ -0,0 +1,32 @@
+/*
+ * ADI 6 Project
+ * Copyright C.S.L. ClaMAN 2017
+ * This code is provided "as is". Modify this thing under your responsability.
+ */
+package com.adi.service.data;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ * @author Caique
+ */
+public abstract class Controller {
+    protected String baseUrl;
+    protected Gson gson;
+
+    public Controller(String baseUrl) {
+        baseUrl = baseUrl.replace("//", "/");
+        baseUrl = baseUrl.replace(":/", "://");
+        this.baseUrl = baseUrl;
+        System.out.println(baseUrl);
+        gson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .setDateFormat("yyyy-MM-dd HH:mm:ss")
+            .create();
+    }
+}
---
+++ src/com/adi/service/data/TagController.java
@@ -0,0 +1,78 @@
+/*
+ * ADI 6 Project
+ * Copyright C.S.L. ClaMAN 2017
+ * This code is provided "as is". Modify this thing under your responsability.
+ */
+package com.adi.service.data;
+
+import com.adi.model.data.Tag;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ * @author Caique
+ */
+public class TagController extends Controller {
+    public TagController(String baseUrl) {
+        super(baseUrl + "/tag");
+    }
+    
+    public Tag find(String name){
+        Tag tag = null;
+        try {
+            HttpURLConnection con = (HttpURLConnection)new URL(baseUrl + "/find?adi_tag=show&name=" + name).openConnection();
+            int response = con.getResponseCode();
+            
+            if (response == HttpURLConnection.HTTP_OK){
+                tag = gson.fromJson(new InputStreamReader(con.getInputStream()), Tag.class);
+            } else {
+                System.out.println(response + " - " + con.getResponseMessage());
+            }
+        } catch (IOException ex){
+            System.err.println(ex.toString());
+        }
+        return tag;
+    }
+    
+    public Tag create(Tag tag){
+        Tag created = null;
+        try {
+            HttpURLConnection con = (HttpURLConnection)new URL(baseUrl).openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setRequestProperty("Content-Type", "application/json");
+            OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream());
+            
+            out.write(gson.toJson(tag, Tag.class));
+            out.close();
+            
+            int response = con.getResponseCode();
+            if (response == HttpURLConnection.HTTP_CREATED){
+                created = gson.fromJson(new InputStreamReader(con.getInputStream()), Tag.class);
+            } else {
+                System.out.println(response + " - " + con.getResponseMessage());
+            }
+        } catch (IOException ex){
+            System.err.println(ex.toString());
+        }
+        return created;
+    }
+    
+    public Tag findOrCreate(String name){
+        Tag tag = find(name);
+        if (tag != null){
+            return tag;
+        } else {
+            tag = new Tag(name);
+            return create(tag);
+        }
+    }
+    
+    public Tag edit(Tag tag){
+        throw new UnsupportedOperationException("Não implementado ainda");
+    }
+}

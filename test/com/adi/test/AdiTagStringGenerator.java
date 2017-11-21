481504b221b2eb03927ace4bd78479c21b0fb9cd
Caique Siqueira <cslclaman@gmail.com>
2017-11-21 02:04:20 -0200

    Start tests with image creation

--- test/com/adi/test/AdiTagStringGenerator.java
+++ test/com/adi/test/AdiTagStringGenerator.java
@@ -8,9 +8,11 @@ package com.adi.test;
 import com.adi.exception.UnspecifiedParameterException;
 import com.adi.instance.Configuration;
 import com.adi.instance.model.Source;
+import com.adi.model.data.Image;
 import com.adi.model.data.Tag;
 import com.adi.model.source.Post;
 import com.adi.model.source.danbooru.DanbooruPost;
+import com.adi.service.data.ImageController;
 import com.adi.service.data.TagController;
 import com.adi.service.file.Archiver;
 import com.adi.service.file.Downloader;
@@ -19,7 +21,12 @@ import com.adi.service.search.Search;
 import com.adi.service.search.SearchTypeInstance;
 import com.adi.service.tags.AdiTagsModel;
 import com.adi.test.utility.ConsoleProgress;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 /**
  *
@@ -27,14 +34,26 @@ import java.io.IOException;
  */
 public class AdiTagStringGenerator {
 
+    public static final String SERVER_URL = "http://localhost:5000/";
+    
     public static void main(String[] args) {
         Configuration conf = Configuration.defaults();
         Archiver archiver = new Archiver(conf.getPreferences().getInputFolders(), true);
         Source source = conf.getDefaultSource();
         Search search = new Search(source);
+        BufferedWriter log;
+        try {
+            log = new BufferedWriter(new FileWriter(new File(Configuration.LOCAL_PATH + "excluded.txt"),true));
+        } catch (IOException ex){
+            System.err.println(ex.toString());
+            log = null;
+        }
+        
+        TagController tc = new TagController(SERVER_URL);
+        ImageController ic = new ImageController(SERVER_URL);
         
         for (Archive a : archiver.getArchiveList()){
-            
+            String origPath = a.getName();
             search.setSearchType(SearchTypeInstance.POSTS, a.getQueryTypeParameter());
             search.setQuery(a.getQueryText());
             try {
@@ -44,8 +63,6 @@ public class AdiTagStringGenerator {
             }
             System.out.println(a.getPath());
             if (search.hasResults()){
-                TagController tc = new TagController("http://localhost:5000/");
-                
                 for (Post p : (Post[])search.resultList()){
                     AdiTagsModel modelAdi = new AdiTagsModel();
                     modelAdi.setSource(source.getId(), p.getId());
@@ -68,6 +85,11 @@ public class AdiTagStringGenerator {
                         try {
                             Archive b = Downloader.download(source.getDataUrl() + p.getFileUrl(), new ConsoleProgress());
                             System.out.println(b.getPath());
+                            if (log != null){
+                                log.write(a.getPath() + "\t" + b.getPath() + "\n");
+                                log.flush();
+                            }
+                            Files.delete(a.toPath());
                             a = b;
                         } catch (IOException ex){
                             System.err.println(ex.getMessage());
@@ -85,12 +107,33 @@ public class AdiTagStringGenerator {
                         }
                     }
                     //Obs.: 220 is the medium limit for file names, but is better to change this to a constant thing.
-                    int nameSize = 220 - modelAdi.getRelativePath().length();
+                    String nameFolder = modelAdi.getFolder(conf.getPreferences().getOutputFolders()[0]);
+                    int nameSize = 220 - nameFolder.length();
                     
                     System.out.println(modelAdi.getTagString());
                     System.out.println(modelAdi.getFolder() + modelAdi.getTagString(nameSize));
                     System.out.println(modelAdi.getRelativePath() + modelAdi.getTagString(nameSize) + "." + a.getExtension());
+                    System.out.println(nameFolder);
                     
+                    Image image = null;
+                    
+                    try {
+                        File pn = new File(nameFolder);
+                        if (!pn.exists()){
+                            pn.mkdirs();
+                        }
+                        File n = new File(pn, modelAdi.getTagString(nameSize) + "." + a.getExtension());
+                        Files.move(a.toPath(), n.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                        Archive na = new Archive(n.getPath());
+                        image = new Image(p, na, modelAdi);
+                        image.setFileSource(origPath);
+                    } catch (IOException ex){
+                        System.err.println(ex.toString());
+                    }
+                    
+                    if (image != null){
+                        ic.create(image);
+                    }
                     
                 }
             }
